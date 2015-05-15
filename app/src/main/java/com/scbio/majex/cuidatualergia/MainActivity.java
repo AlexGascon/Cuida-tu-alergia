c770633853de38c3408e67002b62faee00610c70
MariaJose <majonet6@gmail.com>
2015-05-15 23:28:29 +0200

    Anyadida Emergencia i Consells

--- app/src/main/java/com/scbio/majex/cuidatualergia/MainActivity.java
+++ app/src/main/java/com/scbio/majex/cuidatualergia/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,7 +15,7 @@ import android.widget.Toast;
  */
 public class MainActivity extends Activity{
 
-    Button buttonLoc, buttonConsejos, buttonConfig, buttonSaber;
+    Button buttonLoc, buttonConsejos, buttonConfig, buttonSaber, buttonSalir, buttonEmergencia;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +25,9 @@ public class MainActivity extends Activity{
         buttonSaber = (Button) findViewById(R.id.Boton2);
         buttonConsejos = (Button) findViewById(R.id.Boton3);
         buttonConfig = (Button) findViewById(R.id.Boton4);
+        buttonSalir = (Button) findViewById(R.id.Boton5);
+        buttonEmergencia = (Button) findViewById(R.id.Boton6);
+
 
 
 
@@ -65,8 +66,20 @@ public class MainActivity extends Activity{
             }
         });
 
-    }
+        buttonSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                lanzarSalir();
+            }
+        });
 
+        buttonEmergencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                llamadaEmergencia();
+            }
+        });
+    }
 
 
 
